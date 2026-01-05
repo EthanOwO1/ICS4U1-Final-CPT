293462f2987a32d5b83a5bfbfbe1c020d7539b46
LazyPhilo <118143675+EthanOwO1@users.noreply.github.com>
2026-01-04 21:52:46 -0500

    Created a General Structure for our CPT
    - Enabled Liveshare for real time code collaboration.
    - Created a Main Menu for our battleship game.
    - Created a main panel where main gameplay will be happening with a menu bar.
    - Created an animation file where animation will be programmed.
    
    Co-Authored-By: Carsten Lee <192229102+Carsten-Lee@users.noreply.github.com>
    Co-Authored-By: brandonsu1622 <213062215+brandonsu1622@users.noreply.github.com>

---
+++ animation.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+
+public class animation extends JPanel{
+    // Properties
+
+
+    // Methods
+    public void paintComponent(Graphics g){ 
+        super.paintComponent(g);
+    }
+
+    // Constructors
+    public animation(){
+        super(); 
+    }
+
+}
---
+++ mainProgram.java
@@ -0,0 +1,103 @@
+//Battleship Game
+//version 0.0
+//December 18 2025
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+
+public class mainProgram implements ActionListener, MouseListener{
+  
+  // Properties 
+  JFrame theFrame = new JFrame("Battleship"); 
+  JPanel mainMenuPanel = new JPanel();
+  animation thePanel = new animation();
+  Timer theTimer = new Timer(1000/60, this);
+
+  JMenuBar theMenuBar = new JMenuBar();
+  JMenu theMenu = new JMenu("Menu");
+  JMenuItem instructions = new JMenuItem("Instructions");
+  JMenuItem about = new JMenuItem("About");
+
+  JButton playButton;
+
+
+  // Methods
+  public void actionPerformed(ActionEvent evt){
+      if(evt.getSource() == playButton){
+        theFrame.setContentPane(thePanel);
+        theFrame.pack();
+      }
+  }
+  @Override
+  public void mouseClicked(MouseEvent e) {
+  }
+  @Override
+  public void mousePressed(MouseEvent e) {
+  }
+  @Override
+  public void mouseReleased(MouseEvent e) {
+  }
+  @Override
+  public void mouseEntered(MouseEvent e) {
+  }
+  @Override
+  public void mouseExited(MouseEvent e) {
+  }
+
+  public void mainMenu(){
+    JLabel theTitle = new JLabel("Battleships");
+    playButton = new JButton("Play");
+
+    Font currentFont = theTitle.getFont();
+
+    Font biggerFont = currentFont.deriveFont(currentFont.getSize() + 20f);
+
+    theTitle.setFont(biggerFont);
+    
+    theFrame.setContentPane(mainMenuPanel);
+    mainMenuPanel.setLayout(null);
+
+    theTitle.setSize(200, 50);
+    theTitle.setLocation(550, 50);
+    mainMenuPanel.add(theTitle);
+
+    playButton.setSize(100, 30);
+    playButton.setLocation(600, 300);
+    playButton.addActionListener(this);
+    mainMenuPanel.add(playButton);
+    
+    theFrame.pack();
+  }
+
+  // Constructors 
+  public mainProgram(){
+
+    mainMenu();
+
+    thePanel.setPreferredSize(new Dimension(1280,720)); 
+    thePanel.setLayout(null); 
+    mainMenuPanel.setPreferredSize(new Dimension(1280,720));
+
+    theFrame.setJMenuBar(theMenuBar);
+    theMenuBar.add(theMenu);
+
+    theMenu.add(instructions);
+    theMenu.add(about);
+    
+    theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+    theFrame.pack(); 
+    theFrame.setVisible(true); 
+  }
+
+  // Main Method
+
+  public static void main(String[] args){
+    new mainProgram();
+  }
+
+}
+
+
+  
